565166892fee9c1c4d1d069077ae742dd2700333
Kirk Strauser <kirk@strauser.com>
2018-05-17 23:38:02 -0700

    Oops, sorry, PyPI: no 3.4 for you either

--- setup.py
+++ setup.py
@@ -32,7 +32,6 @@ setup(
         'Topic :: Software Development :: Testing',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: Implementation :: CPython',
